43198baeaef4703b4be293bd1bf0d78105978f26
jpberse <jp.bersezio@gmail.com>
2024-12-14 16:03:33 -0300

    Creacion del componente navbar con sus botonesy estilos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import ButtonsNav from './ButtonsNav';
 
 function Navbar() {
   const total = 25000;
   const token = false;
   return (
-    <nav className='bg-gray-800 flex'>
-        <h2 className='text-white color'>Mamma Mia!</h2>
-        <span>Home</span>
-        <span>Profile</span>
-        <span>Logout</span>
+    <nav className='bg-gray-800 flex justify-between items-center h-12 px-4'>
+      <div className='flex items-center gap-2'>
+        <h2 className='text-white text-2xl'>Mamma Mia!</h2>
+        <ButtonsNav name="Home" icon="🍕"/>
+        { token ? <ButtonsNav name="Profile" icon="🔓" /> : <ButtonsNav name="Login" icon="🔐" /> }
+        { token ? <ButtonsNav name="Logout" icon="🔓" /> : <ButtonsNav name="Register" icon="🔐" /> }
+      </div> 
+      <div>
+        <button className='text-blue-400 text-lg font-light border-solid border border-blue-400/50 rounded-md px-2 py-1'>🛒 Total: ${total.toLocaleString()}</button>
+      </div>
     </nav>
   )
 }
