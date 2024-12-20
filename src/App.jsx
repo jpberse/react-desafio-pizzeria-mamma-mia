b823e3ae610f923a982b95295eb73ab1a82e99c5
jpberse <jp.bersezio@gmail.com>
2024-12-20 19:40:26 -0300

    Hito 2 - Requerimiento 1, creacion de componente para registrarse y crear una cuenta

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Footer from './components/Footer'
+import Register from './components/Register'
 
 function App() {
   return (
     <>
       <div className='box-border m-0 p-0'>
-        <Navbar />
-        <Home />
-        <Footer />
+        <Navbar/>
+        {/* <Home/> */}
+        <Register/>
+        <Footer/>
       </div>
     </>
   )
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,24 +4,24 @@ function Card({ producto }) {
     const { name, price, ingredients, img } = producto
 
 return (
-    <article class="w-full max-w-xl pb-4 bg-[#e0e0e0] rounded-lg"
+    <article className="w-full max-w-xl pb-4 bg-[#e0e0e0] rounded-lg"
         style={{ boxShadow: '15px 15px 25px #bebebe, -15px -15px 25px #ffffff' }}
     >
         <a href="#">
-            <img class="rounded-t-lg " src={img} alt={`Imagen de una pizza ${name}`}/>
+            <img className="rounded-t-lg " src={img} alt={`Imagen de una pizza ${name}`}/>
         </a>
-        <div class="px-5 pb-5 flex flex-col items-center gap-4">
+        <div className="px-5 pb-5 flex flex-col items-center gap-4">
             <a href="#">
-                <h5 class="text-3xl font-semibold text-gray-900 pt-4">Pizza {name}</h5>
+                <h5 className="text-3xl font-semibold text-gray-900 pt-4">Pizza {name}</h5>
             </a>
             <div className="flex flex-col items-center justify-center gap-2">
                 <h4 className="text-gray-600 text-2xl font-light pt-4">Ingredientes:</h4>
                 <span className="text-2xl text-center h-14">🍕 {ingredients.join(", ")}</span>
             </div>
             <span className="text-4xl text-center pt-2 pb-6 border-b-2 border-black/15 w-full max-w-xl">Precio: ${price.toLocaleString()}</span>
-            <div class="flex items-center justify-between gap-24 pt-6">
+            <div className="flex items-center justify-between gap-24 pt-6">
                 <button type='button' className='text-gray-900 text-center bg-[#e0e0e0bc] border-2 border-black/25 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-2xl px-5 py-2.5 me-2 mb-2'>Ver Más 👀</button>
-                <button type='button' class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-2xl px-5 py-2.5 me-2 mb-2">🛒 Añadir</button>
+                <button type='button' className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-2xl px-5 py-2.5 me-2 mb-2">🛒 Añadir</button>
             </div>
         </div>
     </article>
